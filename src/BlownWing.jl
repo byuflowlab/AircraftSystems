ee5b1f756d6429fb7e859e781a27e731159cc80d
rymanderson <rymanderson@gmail.com>
2021-05-12 14:52:50 -0600

    update skeleton

--- src/BlownWing.jl
+++ src/BlownWing.jl
@@ -7,6 +7,10 @@ import VortexLattice
 VL = VortexLattice
 
 # load modules
+include("system.jl")
+include("analysis.jl")
+include("plots.jl")
+
 
 # export functions
 
--- src/analysis.jl
+++ src/analysis.jl
@@ -1,6 +1,6 @@
 # analysis functions
 
-"Calculates lift, drag, and moment coefficients of the system.
+"Calculates overall lift, drag, moment, and rotor coefficients of the system.
 
 Inputs:
 
@@ -10,9 +10,23 @@ Inputs:
 
 Outputs:
 
-* no explicit outputs, but cl, cd, cm, CL, CD, and CM values are computed and stored inside `system.VL_system`.
+* CF::Vector{Array{R,1}} : each vector element is a 1-D array corresponding to each lifting surface in `system.system` and containing [C_D, C_L, C_S]
+* CM::Vector{Array{R,1}} : each vector element is a 1-D array corresponding to each lifting surface in `system.system` and containing [C_Mx, C_My, C_Mz]
+* CRotor::Vector{Tuple{R,R,R}} : each vector element is a 3-Tuple corresponding to each rotor and containing (C_T, C_Q, eta)
 
 "
 function solve!(system::System, freestream::VL.Freestream, control::Vector{R}) where R
+
+end
+
+"
+Returns local lift, drag, and moment coefficients of the system.
+
+Outputs:
+
+* cf::Vector{Array{R,2},1} : each vector element is a 2-D array corresponding to each lifting surface in `system.system` and containing force coefficients (cd, cl, cs) for each spanwise station
+* cm::Vector{Array{R,2},1} : each vector element is a 2-D array corresponding to each lifting surface in `system.system` and containing moment coefficients for each spanwise station
+"
+function forcedistribution(system::System)
 
 end
