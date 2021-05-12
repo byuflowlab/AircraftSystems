ddd62ae3213abe03cd7ef2fafe12c5ac7e9bb5e0
rymanderson <rymanderson@gmail.com>
2021-05-12 14:58:43 -0600

    update skeleton

--- src/analysis.jl
+++ src/analysis.jl
@@ -10,9 +10,7 @@ Inputs:
 
 Outputs:
 
-* CF::Vector{Array{R,1}} : each vector element is a 1-D array corresponding to each lifting surface in `system.system` and containing [C_D, C_L, C_S]
-* CM::Vector{Array{R,1}} : each vector element is a 1-D array corresponding to each lifting surface in `system.system` and containing [C_Mx, C_My, C_Mz]
-* CRotor::Vector{Tuple{R,R,R}} : each vector element is a 3-Tuple corresponding to each rotor and containing (C_T, C_Q, eta)
+* all \"outputs\" are stored in the system object
 
 "
 function solve!(system::System, freestream::VL.Freestream, control::Vector{R}) where R
@@ -20,12 +18,35 @@ function solve!(system::System, freestream::VL.Freestream, control::Vector{R}) w
 end
 
 "
+Returns overall lift, drag, moment, thrust, and torque coefficients and rotor efficiencies of the system.
+
+NOTE: to be run AFTER calling `solve!()`
+
+Inputs:
+
+* `system::System : `BlownWing.System` object
+
+Outputs:
+
+* CF::Vector{Array{R,1}} : each vector element is a 1-D array corresponding to each lifting surface in `system.system` and containing [C_D, C_L, C_S]
+* CM::Vector{Array{R,1}} : each vector element is a 1-D array corresponding to each lifting surface in `system.system` and containing [C_Mx, C_My, C_Mz]
+* CRotor::Vector{Tuple{R,R,R}} : each vector element is a 3-Tuple corresponding to each rotor and containing (C_T, C_Q, eta)
+
+"
+function forcecoefficients(system::System)
+
+end
+
+"
 Returns local lift, drag, and moment coefficients of the system.
+
+NOTE: to be run AFTER calling `solve!()`
 
 Outputs:
 
 * cf::Vector{Array{R,2},1} : each vector element is a 2-D array corresponding to each lifting surface in `system.system` and containing force coefficients (cd, cl, cs) for each spanwise station
 * cm::Vector{Array{R,2},1} : each vector element is a 2-D array corresponding to each lifting surface in `system.system` and containing moment coefficients for each spanwise station
+
 "
 function forcedistribution(system::System)
 
